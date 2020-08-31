3418b36bdfa1096f223c2c5177ae76d99fec7e1a
felipe.lima <felipena.lima@gmail.com>
2020-08-31 11:51:32 -0300

    fix: fixed bugs

--- routes/gradeRouter.js
+++ routes/gradeRouter.js
@@ -3,11 +3,11 @@ import controller from '../controllers/gradeController.js';
 
 const app = express();
 
-app.post('/grades', controller.create);
-app.get('/grades', controller.findAll);
-app.get('/grades/:id', controller.findOne);
-app.put('/grades/:id', controller.update);
-app.delete('/grades/:id', controller.remove);
-app.delete('/grades', controller.removeAll);
+app.post('/grade', controller.create);
+app.get('/grade', controller.findAll);
+app.get('/grade/:id', controller.findOne);
+app.put('/grade/:id', controller.update);
+app.delete('/grade/:id', controller.remove);
+app.delete('/grade', controller.removeAll);
 
 export { app as gradeRouter };
