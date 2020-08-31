459cc29299b732db3f3b32996db341ef3be7547f
felipe.lima <felipena.lima@gmail.com>
2020-08-31 13:55:31 -0300

    fix: fixed environment vars

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://gradesapi-challenge.herokuapp.com:8080',
+    origin: 'https://gradesapi-challenge.herokuapp.com',
   })
 );
 
